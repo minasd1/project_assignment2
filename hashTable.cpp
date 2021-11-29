847a935c03b7f732b1606ea8037bb22c1228487e
minasd <minas.dsp@gmail.com>
2021-11-29 20:35:07 +0200

    Added hashTable.cpp/h - used for LSH

---
+++ hashTable.cpp
@@ -0,0 +1,69 @@
+#include "hashTable.h"
+
+void hashTable_initialization(int num_of_hashTables, int num_of_buckets){
+
+    //INITIALIZE L HASHTABLES WITH HASHTABLESIZE BUCKETS AND 1 CURVE (WITH ID 0) IN EACH BUCKET
+    HashTables.resize(num_of_hashTables, vector<vector<int> >(num_of_buckets, vector<int>(0)));
+    //IT CAN ALSO BE INITIALIZED AS ZERO
+}
+
+//PUSH A CURVE'S ID VALUE TO A BUCKET OF EACH OF THE HASHTABLES USING G FUNCTION 
+//EACH BUCKET IS INDICATED BY THE INT VALUES PRODUCED BY FUNCTION G
+void hashTable_push_back(vector<int>& g, int key_val){
+
+    for(int i = 0; i < HashTables.size(); i++){
+        HashTables[i][g[i]].push_back(key_val);
+        //HashTables[i][g[i]].end() = key_val;
+    }
+}
+
+//GET KEY VALUES OF ALL THE CURVES OF A BUCKET OF EACH HASH TABLE
+//EACH BUCKET IS INDICATED BY THE INT VALUES PRODUCED BY FUNCTION G 
+vector<int> hashTable_get_curves_in_buckets(vector<int>& g){
+
+    vector<int> points_in_buckets;
+
+    for(int i = 0; i < HashTables.size(); i++){
+        for(int j = 0; j < HashTables[i][g[i]].size(); j++){
+            //INSERT THE NUMBER OF HASHTABLE OF THE CURVE (USE TO GET APPROPRIATE ID VALUE)
+            points_in_buckets.push_back(i);
+            //PUSH THE ID OF THE CURVE TO POINTS IN HASHTABLE BUCKETS
+            points_in_buckets.push_back(HashTables[i][g[i]][j]);
+        }
+    }
+
+    return points_in_buckets;
+}
+
+//GET NUMBER OF HASHTABLES - L
+int hashTable_get_num_of_htables(){
+
+    return HashTables.size();
+}
+
+//GET NUMBER OF CURVES IN A HASHTABLE BUCKET
+int hashTable_get_bucket_size(int htable_num, int bucket){
+
+    return HashTables[htable_num][bucket].size();
+}
+
+//GET A SPECIFIC CURVE FROM A HASHTABLE BUCKET
+int hashTable_get_curve(int htable_num, int bucket, int place){
+
+    return HashTables[htable_num][bucket][place];
+}
+
+//PRINT HASHTABLES DATA - USED FOR CHECKING PURPOSES
+void hashTable_print_data(){
+    for (int i = 0; i < HashTables.size(); i++) {
+        for (int j = 0; j < HashTables[i].size(); j++){
+            for(int k = 0; k < HashTables[i][j].size(); k++){
+                cout<< HashTables[i][j][k] << " ";
+            }
+        }
+        cout << endl;
+    }
+    cout << "Hashtable size is: " << HashTables.size() << endl;
+    cout << "buckets of each hashtable are: " << HashTables[0].size() << endl;
+    cout << "elements in each bucket are: " << HashTables[0][0].size() << endl;
+}
---
+++ hashTable.h
@@ -0,0 +1,39 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static vector<vector <vector <int> > > HashTables;  //DECLARE A VECTOR OF HASHTABLES
+
+void hashTable_initialization(int num_of_hashTables, int num_of_buckets);
+void hashTable_push_back(vector<int>& g, int key_val);
+vector<int> hashTable_get_curves_in_buckets(vector<int>& g);
+int hashTable_get_num_of_htables();
+int hashTable_get_bucket_size(int htable_num, int bucket);
+int hashTable_get_curve(int htable_num, int bucket, int place);
+void hashTable_print_data();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
